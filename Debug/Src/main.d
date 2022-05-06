a13b83d17e61a5e13bb9427d9ce7581caf0589a7
bilgehanparay <bilgehanparay@gmail.com>
2022-05-07 00:32:32 +0300

    driver file added

---
+++ Debug/Src/main.d
@@ -0,0 +1,3 @@
+Src/main.o: ../Src/main.c \
+ /home/bilgehan/Projects/MasteringMCUAndEmbeddedDriverDevelopment/Codes/Project1/stm32f4xx_drivers/drivers/Inc/stm32f407xx.h
+/home/bilgehan/Projects/MasteringMCUAndEmbeddedDriverDevelopment/Codes/Project1/stm32f4xx_drivers/drivers/Inc/stm32f407xx.h:
---
+++ drivers/Inc/stm32f407xx.h
@@ -0,0 +1,94 @@
+/*
+ * stm32f407xx.h
+ *
+ *  Created on: May 6, 2022
+ *      Author: bilgehan
+ */
+
+#ifndef INC_STM32F407XX_H_
+#define INC_STM32F407XX_H_
+
+/**
+ * Base addresses of flash and SRAM memories
+ * */
+#define FLASH_BASEADDR		0x08000000U /**/
+#define SRAM1_BASEADDR		0x20000000U /**/
+#define SRAM2_BASEADDR		0x20001C00U /**/
+#define ROM					0x1FFF0000U /**/
+#define SRAM				SRAM1_BASEADDR /**/
+
+/**
+ * Base addresses of bus domains
+ * */
+#define PERIPH_BASE			0x40000000U;
+#define APB1PERIPH_BASE		PERIPH_BASE;
+#define APB2PERIPH_BASE		0x40010000U;
+#define AHB1PERIPH_BASE		0x40020000U;
+#define AHB2PERIPH_BASE		0x50000000U;
+
+/**
+ * Base addresses of AHB1
+ * */
+#define GPIOA_BASEADDR		(AHB1PERIPH_BASE+0x0000)
+#define GPIOB_BASEADDR		(AHB1PERIPH_BASE+0x0400)
+#define GPIOC_BASEADDR		(AHB1PERIPH_BASE+0x0800)
+#define GPIOD_BASEADDR		(AHB1PERIPH_BASE+0x0C00)
+#define GPIOE_BASEADDR		(AHB1PERIPH_BASE+0x1000)
+#define GPIOF_BASEADDR		(AHB1PERIPH_BASE+0x1400)
+#define GPIOG_BASEADDR		(AHB1PERIPH_BASE+0x1800)
+#define GPIOH_BASEADDR		(AHB1PERIPH_BASE+0x1C00)
+#define GPIOI_BASEADDR		(AHB1PERIPH_BASE+0x2000)
+#define GPIOJ_BASEADDR		(AHB1PERIPH_BASE+0x2400)
+#define GPIOK_BASEADDR		(AHB1PERIPH_BASE+0x2800)
+
+
+/**
+ * Base addresses of AHB2
+ * */
+
+/**
+ * Base addresses of APB1
+ * */
+#define I2C1_BASEADDR		(APB1PERIPH_BASE + 0x5400)
+#define I2C2_BASEADDR		(APB1PERIPH_BASE + 0x5800)
+#define I2C3_BASEADDR		(APB1PERIPH_BASE + 0x5C00)
+#define SPI2_BASEADDR		(APB1PERIPH_BASE + 0x3800)
+#define SPI3_BASEADDR		(APB1PERIPH_BASE + 0x3C00)
+#define USART2_BASEADDR		(APB1PERIPH_BASE + 0x4400)
+#define USART3_BASEADDR		(APB1PERIPH_BASE + 0x4800)
+#define UART4_BASEADDR		(APB1PERIPH_BASE + 0x4C00)
+#define UART5_BASEADDR		(APB1PERIPH_BASE + 0x5000)
+
+/**
+ * Base addresses of APB2
+ * */
+#define EXTI_BASEADDR		(APB2PERIPH_BASE + 0x3C00)
+#define SPI1_BASEADDR		(APB2PERIPH_BASE + 0x3000)
+#define USART1_BASEADDR		(APB2PERIPH_BASE + 0x1000)
+#define USART6_BASEADDR		(APB2PERIPH_BASE + 0x1400)
+#define SYSCFG_BASEADDR		(APB2PERIPH_BASE + 0x3800)
+
+#endif /* INC_STM32F407XX_H_ */
+
+//TODO: define base address of other devices on this bus
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
